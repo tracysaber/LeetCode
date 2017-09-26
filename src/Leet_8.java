1e453374354cfbf6670bb0d9d1af133f5963d4bc
tracysaber <402807046@qq.com>
2017-09-26 22:48:33 +0800

    8

--- src/Leet_8.java
+++ src/Leet_8.java
@@ -9,19 +9,40 @@
  The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
  */
 public class Leet_8 {
-    public static void main(String s){
-        boolean flag = true;
+    public int myAtoi(String str) {
+        str=str.trim();
+        if(str.length()==0)
+            return 0;
+        int sign =1;
         int start=0;
-        if(s.charAt(0)=='+') {
-            flag = true;
+        if(str.charAt(0)=='-') {
+            sign = -1;
             start++;
         }
-        if(s.charAt(0)=='-') {
-            flag = false;
+        else if(str.charAt(0)=='+') {
+            sign=1;
             start++;
         }
-        for(int i=start;i<s.length();i++){
-               
+        long sum=0;
+        for(int i=start;i<str.length();i++){
+            char a=str.charAt(i);
+            if(!Character.isDigit(a)){
+                return (int) sum * sign;
+            }
+            sum=sum*10+(a-'0') ;
+
+            if(sum>Integer.MAX_VALUE&&sign>0){
+                return Integer.MAX_VALUE;
+            }
+            if((-1)*sum<Integer.MIN_VALUE&&sign<0){
+                return Integer.MIN_VALUE;
+            }
+
         }
+        return (int)sum * sign;
+    }
+    public static void main(String args[]){
+        String s ="    10522545459";
+        System.out.println(new Leet_8().myAtoi(s));
     }
 }
