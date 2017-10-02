e4b193da14f7f55cd19d941b3072137e9fc22f7d
tracysaber <402807046@qq.com>
2017-10-03 00:07:27 +0800

    14

--- src/Leet_13.java
+++ src/Leet_13.java
@@ -1,35 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by tracysaber on 2017-10-1.
  * Write a function to find the longest common prefix string amongst an array of strings.
  */
 public class Leet_13 {
     public String longestCommonPrefix(String[] strs) {
-
-        int len = strs.length;
-        if (len==0)
-            return "";
-        int min = strs[0].length();
-        for(int i =0;i<len;i++){
-            if(strs[i].length()<min)
-                min=strs[i].length();
-        }
-        int count=0;
-        for(int i =0;i<min;i++){
-            boolean flag =true;
-            count=i;
-            for(int j=1;j<len;j++){
-                //boolean flag =true;
-                if(strs[j].charAt(i)!=strs[0].charAt(i)) {
-                    flag = false;
-                    break;
-                }
+        if(strs.length>0){
+            Arrays.sort(strs);
+            int len =Math.min(strs[0].length(),strs[strs.length-1].length());
+            StringBuilder ss= new StringBuilder();
+            for(int i=0;i<len;i++){
+                if(strs[0].charAt(i)==strs[strs.length-1].charAt(i))
+                    ss.append(strs[0].charAt(i));
+                else
+                    return ss.toString();
             }
-            if(!flag) {
-                count=i;
-                break;
-            }
+            return ss.toString();
         }
-        return strs[0].substring(0,count+1);
+        return "";
     }
     public static void main(String args[]){
         String strs[]={"you","yo","yoyoyo"};
