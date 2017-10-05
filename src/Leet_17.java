22b6c74be15123bb7cea0edfefd2fc592f622c85
tracysaber <402807046@qq.com>
2017-10-05 22:29:25 +0800

    17

--- src/Leet_17.java
+++ src/Leet_17.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,9 +16,36 @@ import java.util.List;
  */
 public class Leet_17 {
     public List<String> letterCombinations(String digits) {
+        String digit[]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> re = new LinkedList<String>();
+        if(digits.length()==0)
+            return re;
+        if (digits.length()==1){
+            if(Character.isDigit(digits.charAt(0))) {
+                String c = digit[digits.charAt(0)-'0'-1];
+                for(int i=0;i<c.length();i++){
+                    re.add(String.valueOf(c.charAt(i)));
+                }
+                return re;
+            }
+            return re;
+        }
+        else{
+            String c=digit[digits.charAt(digits.length()-1)-'0'-1];
+            List<String> pre =letterCombinations(digits.substring(0,digits.length()-1));
+            for(int i=0;i<pre.size();i++){
+                String base = pre.get(i);
+                for(int j=0;j<c.length();j++){
+                    re.add(base+c.charAt(j));
+                }
+            }
+            return re;
+
+        }
 
     }
     public static void main(String args[]){
+        List<String> a= new Leet_17().letterCombinations("23");
 
     }
 }
---
+++ src/Leet_18.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * Created by tracysaber on 2017-10-5.
+ * Given an array S of n integers, are there elements a, sb, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+ Note: The solution set must not contain duplicate quadruplets.
+
+ For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+
+ A solution set is:
+ [
+ [-1,  0, 0, 1],
+ [-2, -1, 1, 2],
+ [-2,  0, 0, 2]
+ ]
+ */
+public class Leet_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+    }
+}
