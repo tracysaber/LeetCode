f15d301755c67560f4c772fdfaad37f3ed8df04a
tracysaber <402807046@qq.com>
2017-10-27 00:09:32 +0800

    10

--- src/Leet_10.java
+++ src/Leet_10.java
@@ -19,8 +19,20 @@
  */
 public class Leet_10 {
     public boolean isMatch(String s, String p) {
-        boolean flag = true;
-        return true;
-       // for(int i=0;)
+        if(p.isEmpty()) return s.isEmpty();
+        if(p.length()==1)
+            return (s.length()==1&&(p.charAt(0)=='.'||p.charAt(0)==s.charAt(0)));
+        if(p.charAt(1)!='*'){
+            if(s.isEmpty())  return false;
+            return (s.charAt(0)==p.charAt(0)||p.charAt(0)=='.')&&isMatch(s.substring(1),p.substring(1));
+        }
+        while(!s.isEmpty()&&(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.')){
+            if(isMatch(s,p.substring(2))) return true;
+            s=s.substring(1);
+        }
+        return isMatch(s,p.substring(2));
+    }
+    public static void main(String args[]){
+        System.out.println(new Leet_10().isMatch("a",".*..a*"));
     }
 }
