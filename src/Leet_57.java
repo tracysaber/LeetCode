2cfbb10aa64d5b97213c37095731fc6cc15acc52
tracysaber <402807046@qq.com>
2017-11-03 10:09:45 +0800

    57

--- src/Leet_57.java
+++ src/Leet_57.java
@@ -20,17 +20,88 @@ import java.util.List;
 public class Leet_57 {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         List<Interval> result = new LinkedList<Interval>();
+        if(intervals.size()==0){
+            result.add(newInterval);
+            return result;
+        }
         int start = intervals.get(0).start;
         int end = intervals.get(0).end;
         int newstart = newInterval.start;
         int newend = newInterval.end;
+        boolean flag = false;
+//        for(Interval interval:intervals){
+//
+//        }
         for(Interval interval:intervals){
-            if(interval.start>newstart) {
-                start = newstart;
-                end = Math.max(end,newend);
+            if(!flag) {
+                if(newstart<=start){
+                    flag = true;
+                    if(newend<start){
+                        result.add(new Interval(newstart,newend));
+                    }
+                    else{
+                        start = newstart;
+                        end = Math.max(newend,end);
+                    }
+                }
+                else{
+                    if(newstart<=end){
+                        flag =true;
+                        end=Math.max(newend,end);
+                    }
+                }
+            }
+
+            if(interval.start>end){
+                result.add(new Interval(start,end));
+                start =interval.start;
+                end = interval.end;
+            }
+            else{
+                end = Math.max(end,interval.end);
             }
 
         }
+        if(!flag) {
+            if(newstart<start){
+                if(newend<start){
+                    result.add(newInterval);
+                    result.add(new Interval(start,end));
+                    return  result;
+                }
+                else{
+                    start = newstart;
+                    end =Math.max(end,newend);
+                    result.add(new Interval(start,end));
+                    return  result;
+                }
+            }
+            if(newstart>end){
+                result.add(new Interval(start,end));
+                result.add(newInterval);
+                return result;
+            }
+            else{
+                end = Math.max(end,newend);
+                result.add(new Interval(start,end));
+                return result;
+            }
+        }
+        else{
+            result.add(new Interval(start,end));
+        }
+
         return  result;
+    }
+    public static void main(String args[]){
+        List<Interval> a = new LinkedList<Interval>();
+        a.add(new Interval(0,5));
+        a.add(new Interval(6,16));
+//        a.add(new Interval(0,2));
+//        a.add(new Interval(3,5));
+//        a.add(new Interval(6,8));
+//        a.add(new Interval(10,12));
+//        a.add(new Interval(13,16));
+        List<Interval> b = new Leet_57().insert(a,new Interval(6,11));
     }
 }
