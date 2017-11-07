c09c47ce48f2027f8c565c66ac2f47d54f4c8935
tracysaber <402807046@qq.com>
2017-11-07 23:40:28 +0800

    67

---
+++ src/Leet_67.java
@@ -0,0 +1,41 @@
+/**
+ * Created by tracysaber on 2017-11-7.
+ * Given two binary strings, return their sum (also a binary string).
+
+ For example,
+ a = "11"
+ b = "1"
+ Return "100".
+ */
+public class Leet_67 {
+    public String addBinary(String a, String b) {
+        if(a == null || a.isEmpty()) {
+            return b;
+        }
+        if(b == null || b.isEmpty()) {
+            return a;
+        }
+        char[] aArray = a.toCharArray();
+        char[] bArray = b.toCharArray();
+        StringBuilder stb = new StringBuilder();
+
+        int i = aArray.length - 1;
+        int j = bArray.length - 1;
+        int aByte;
+        int bByte;
+        int carry = 0;
+        int result;
+
+        while(i > -1 || j > -1 || carry == 1) {
+            aByte = (i > -1) ? Character.getNumericValue(aArray[i--]) : 0;
+            bByte = (j > -1) ? Character.getNumericValue(bArray[j--]) : 0;
+            result = aByte ^ bByte ^ carry;
+            carry = ((aByte + bByte + carry) >= 2) ? 1 : 0;
+            stb.append(result);
+        }
+        return stb.reverse().toString();
+    }
+    public static void main(String args[]){
+        System.out.println(new Leet_67().addBinary("110","1"));
+    }
+}
