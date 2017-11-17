018a7108dc83e32f59319ea74c6727cb29b206dc
tracysaber <402807046@qq.com>
2017-11-17 09:53:24 +0800

    75

---
+++ src/Leet_78.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+ Note: The solution set must not contain duplicate subsets.
+
+ For example,
+ If nums = [1,2,3], a solution is:
+
+ [
+ [3],
+ [1],
+ [2],
+ [1,2,3],
+ [1,3],
+ [2,3],
+ [1,2],
+ []
+ ]
+
+ */
+public class Leet_78 {
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrack(list, new ArrayList<Integer>(), nums, 0);
+		return list;
+	}
+
+	private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+		list.add(new ArrayList<>(tempList));
+		for(int i = start; i < nums.length; i++){
+			tempList.add(nums[i]);
+			backtrack(list, tempList, nums, i + 1);
+			tempList.remove(tempList.size() - 1);
+		}
+	}
+	public static void main(String []args){
+		int a[]={1,2,3};
+		List<List<Integer>> re = new Leet_78().subsets(a);
+	}
+}
