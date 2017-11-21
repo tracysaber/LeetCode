076888d67c16154cb6e259f4af575552f0f78b8a
tracysaber <402807046@qq.com>
2017-11-21 23:23:57 +0800

    chaloubuque

--- src/Leet_50.java
+++ src/Leet_50.java
@@ -3,7 +3,22 @@
  *Implement pow(x, n).
  */
 public class Leet_50 {
-	public double myPow(double x, int n) {
-		return 0;
+	public double myPow(double x, int m) {
+		double temp = x;
+		if (m == 0)
+			return 1;
+		temp = myPow(x, m / 2);
+		if (m % 2 == 0)
+			return temp * temp;
+		else {
+			if (m > 0)
+				return x * temp * temp;
+			else
+				return (temp * temp) / x;
+		}
+	}
+	public static void main(String args[]){
+		System.out.println(new Leet_50().myPow(2.00000
+				,-2147483647));
 	}
 }
---
+++ src/Leet_52.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tracysaber on 2017-11-21.
+ */
+public class Leet_52 {
+}
