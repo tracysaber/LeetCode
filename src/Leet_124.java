e8150cf9db2769ab1f9717e29f009b9a50b84aaf
tracysaber <402807046@qq.com>
2017-12-21 09:23:50 +0800

    124

---
+++ src/Leet_124.java
@@ -0,0 +1,38 @@
+/**
+ * Given a binary tree, find the maximum path sum.
+
+ For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+ For example:
+ Given the below binary tree,
+
+ 1
+ / \
+ 2   3
+ Return 6.
+ */
+public class Leet_124 {
+	int maxValue;
+
+	public int maxPathSum(TreeNode root) {
+		maxValue = Integer.MIN_VALUE;
+		maxPathDown(root);
+		return maxValue;
+	}
+
+	private int maxPathDown(TreeNode node) {
+		if (node == null) return 0;
+		int left = Math.max(0, maxPathDown(node.left));
+		int right = Math.max(0, maxPathDown(node.right));
+		maxValue = Math.max(maxValue, left + right + node.val);
+		return Math.max(left, right) + node.val;
+	}
+	public static void main(String args[]){
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.left.left = new TreeNode(3);
+		root.left.left.left = new TreeNode(4);
+		root.left.left.left.left = new TreeNode(5);
+		System.out.println(new Leet_124().maxPathSum(root));
+	}
+}
