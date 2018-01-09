573b64c61abf1576d4d127cda3b68fc9a6d551ce
tracysaber <402807046@qq.com>
2018-01-09 23:12:57 +0800

    145

---
+++ src/Leet_145.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+/**
+ * Created by tracysaber on 2018-1-9.
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+
+ For example:
+ Given binary tree {1,#,2,3},
+ 1
+ \
+ 2
+ /
+ 3
+ return [3,2,1].
+ */
+public class Leet_145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+    }
+}
