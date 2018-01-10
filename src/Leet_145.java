99753090b4754e34a27c538f5f7d5a48aa7d4e9a
tracysaber <402807046@qq.com>
2018-01-10 09:10:07 +0800

    145

--- src/Leet_145.java
+++ src/Leet_145.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by tracysaber on 2018-1-9.
@@ -15,6 +15,29 @@ import java.util.List;
  */
 public class Leet_145 {
     public List<Integer> postorderTraversal(TreeNode root) {
-
+        List<Integer> result = new LinkedList<Integer>();
+        if(root==null)  return result;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Set<TreeNode> arrived = new HashSet<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode now = stack.pop();
+            if(!arrived.contains(now)){
+                arrived.add(now);
+                stack.push(now);
+                if(now.right!=null) stack.push(now.right);
+                if(now.left!=null) stack.push(now.left);
+            }
+            else{
+                result.add(now.val);
+            }
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        List<Integer> a = new Leet_145().postorderTraversal(root);
     }
 }
