5bc6eca00dee2c6dddecee5ac42dfe8c505d9ac2
tracysaber <402807046@qq.com>
2018-04-23 10:58:15 +0800

    main

--- src/Main4.java
+++ src/Main4.java
@@ -1,20 +1,30 @@
 
 import java.util.*;
 public class Main4 {
-//    public void findXY(long a){
-//        long y=1;
-//        while(a%2==0){
-//            y *=2;
-//            a=a/2;
-//        }
-//        if(y>=2)
-//            System.out.println(a+" "+y);
-//        else
-//            System.out.println("No");
-//    }
+    public void getSolutions(int a[],int start,int end,int m,List<Integer> result){
+        if(m==0||(start==end&&m<=a[start]))    {result.add(1);return;}
+        for(int i=0;i<=a[start];i++){
+            if(m-i>=0&&end>start){
+                getSolutions(a,start+1,end,m-i,result);
+            }
+        }
+
+    }
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        int t =scanner.nextInt();
-
+        int n =scanner.nextInt();
+        int m =scanner.nextInt();
+        int l[] =new int[n];
+        int r[] =new int[n];
+        int g[] =new int [n];
+        for(int i=0;i<n;i++){
+            l[i] = scanner.nextInt();
+            r[i] = scanner.nextInt();
+            g[i] = r[i]-l[i];
+            m -= l[i];
+        }
+        List<Integer> a =new LinkedList<Integer>();
+        new Main4().getSolutions(g,0,n-1,m, a);
+        System.out.print(a.size());
     }
 }
