0f8b8be4c3b01d15b5a5a52f8989381ab4b48efe
tracysaber <402807046@qq.com>
2018-07-11 17:44:00 +0800

    4

--- src/Leet_130.java
+++ src/Leet_130.java
@@ -17,6 +17,31 @@
 
  */
 public class Leet_130 {
+	public boolean findO(char [][]b,int x,int y){
+		int row_len = b.length;
+		int col_len = b[0].length;
+		if(x==0||x==row_len-1||y==0||y==col_len-1){
+			//b[x][y]='X';
+			return true;
+		}
+
+		if(x>0&&b[x-1][y]=='O'){
+			b[x][y]='X';
+			if(findO(b,x-1,y)){
+				b[x][y]='O';
+			}
+		}
+		if(x>0&&y>0&&b[x+1][y+1]=='O'){
+
+		}
+		if(x>0&&y>0&&b[x+1][y+1]=='O'){
+
+		}
+		if(x>0&&y>0&&b[x+1][y+1]=='O'){
+
+		}
+		return false;
+	}
 	public void solve(char[][] board) {
 		char [][] result = new char[board.length][board[0].length];
 		//for(int i=0;i<)
--- src/Main4.java
+++ src/Main4.java
@@ -1,12 +1,32 @@
 
 import java.util.*;
 public class Main4 {
+    public void printNow(String now,int n){
+        if(n>0){
+            for(int i=0;i<=9;i++){
+                char add = (char) ('0'+i);
+                if(now.length()==0&&i==0)   printNow(now,n-1);
+                else printNow(now+add,n-1);
+
+            }
+        }
+        else{
+            System.out.println(now);
+        }
+    }
+    public void Print(int [][]num){
+        for(int i=0;i<num.length;i++){
+            for(int j=0;j<num[0].length;j++){
+                if(num[i][j]%3==0)  num[i][j]=0;
+            }
+        }
+    }
 
     public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);
-        Long x = scanner.nextLong();
-        if(x==125000000)    System.out.print(1);
-        if(x==250000001)  System.out.print(1);
-        else System.out.print(-1);
+        int [][]a ={{1,2,3},{4,5,6},{7,8,9}};
+        new Main4().Print(a);
+        System.out.println();
+        //new Main4().printNow("",4);
+
     }
 }
