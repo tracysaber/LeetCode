c04e9019d8ae42528f62b6513a9c3258602d0024
tracysaber <402807046@qq.com>
2018-07-22 22:39:50 +0800

    pin

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,118 @@
 import java.util.*;
 
 public class Main{
-    public boolean judge(String a){
-        Stack<Character> stack = new Stack<Character>();
-        stack.push('(');
-        stack.push('(');
-        for(int i=0;i<a.length();i++){
-            char now = a.charAt(i);
-            if(now=='('){
-                stack.push(now);
-            }
-            if(now==')'){
-                if(stack.isEmpty()){
-                    return false;
-                }
-                else
-                    stack.pop();
-            }
-        }
-        if(stack.size()==2)
-            return true;
-        else
-            return false;
-    }
+//    public boolean judge(String a){
+//        Stack<Character> stack = new Stack<Character>();
+//        stack.push('(');
+//        stack.push('(');
+//        for(int i=0;i<a.length();i++){
+//            char now = a.charAt(i);
+//            if(now=='('){
+//                stack.push(now);
+//            }
+//            if(now==')'){
+//                if(stack.isEmpty()){
+//                    return false;
+//                }
+//                else
+//                    stack.pop();
+//            }
+//        }
+//        if(stack.size()==2)
+//            return true;
+//        else
+//            return false;
+//    }
     public static void main(String args[]){
+//        Scanner scan = new Scanner(System.in);
+//        int  t = scan.nextInt();
+//        String s[] =new String[t];
+//        for(int i=0;i<t;i++){
+//            s[i]=scan.next();
+//        }
+//        for(int i=0;i<t;i++){
+//            if(new Main().judge(s[i]))
+//                System.out.println("Yes");
+//            else
+//                System.out.println("No");
+//        }
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        String names [] ={"Alice", "Bob", "Cathy", "Dave"};
+//        int max = 4;
+//        while(n>max){
+//            n = n-max;
+//            max = max<<1;
+//        }
+//        max = max>>2;
+//        double count = Math.ceil(n/(double)max);
+//        System.out.print(names[(int)count-1]);
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        int m = scan.nextInt();
+//        int r[][] = new int[n][n];
+//        for(int i=0;i<m;i++){
+//            String vote = scan.next();
+//            for(int j =0;j<n;j++){
+//                for(int k=j+1;k<n;k++){
+//                    if(vote.charAt(j)<vote.charAt(k)){
+//                        r[j][k]++;
+//                        r[k][j]--;
+//                    }
+//                    if(vote.charAt(j)>vote.charAt(k)){
+//                        r[j][k]--;
+//                        r[k][j]++;
+//                    }
+//                }
+//                //result[j][vote.charAt(j)-'a']++;
+//            }
+//        }
+//        boolean flag= false;
+//        for(int i=0;i<n;i++){
+//            boolean a =true;
+//            for(int j=0;j<n;j++){
+//                if(i!=j&&r[i][j]<=0) {
+//                    a = false;
+//                    break;
+//                }
+//            }
+//            if(a){
+//                System.out.print(i);
+//                flag = true;
+//                break;
+//            }
+//        }
+//        if(!flag)
+//            System.out.print("-1");
+
         Scanner scan = new Scanner(System.in);
-        int  t = scan.nextInt();
-        String s[] =new String[t];
-        for(int i=0;i<t;i++){
-            s[i]=scan.next();
+        int h[] = new int[1024];
+        int i=0;
+        while(scan.hasNext()){
+            h[i++] = scan.nextInt();
         }
-        for(int i=0;i<t;i++){
-            if(new Main().judge(s[i]))
-                System.out.println("Yes");
-            else
-                System.out.println("No");
+        Arrays.sort(h, 0, i);
+        int low=0,high = i-1;
+        int nums =0;
+        while(h[low]==100){
+            low++;
         }
+        nums = low/3;
+        low = 3*nums;
+        while(low<high){
+            if(h[low]+h[high]>300) {
+                nums++;
+                high--;
+            }
+            else{
+                nums++;
+                high--;
+                low++;
+            }
+        }
+        if(low==high)
+            System.out.print(++nums);
+        else
+            System.out.print(nums);
     }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -28,34 +28,62 @@
 //}
 import java.util.*;
 public class Main2 {
-	public  void build(List<String> re,String now,int left,int right){
-		if(left==0&&right==0){
-			re.add(now);
+//	public  void build(List<String> re,String now,int left,int right){
+//		if(left==0&&right==0){
+//			re.add(now);
+//		}
+//		else{
+//			if(left>0)	{
+//				now=now+"(";
+//				build(re,now,left-1,right);
+//				now=now.substring(0,now.length()-1);
+//			}
+//			if(right>0&&right>left){
+//				now=now+")";
+//				build(re,now,left,right-1);
+//			}
+//
+//		}
+//	}
+	public int getCost(String a,String b){
+		int cost =0;
+		for(int i=0;i<a.length();i++){
+			cost+=Math.abs(a.charAt(i)-b.charAt(i));
 		}
-		else{
-			if(left>0)	{
-				now=now+"(";
-				build(re,now,left-1,right);
-				now=now.substring(0,now.length()-1);
-			}
-			if(right>0&&right>left){
-				now=now+")";
-				build(re,now,left,right-1);
-			}
-
-		}
+		return cost;
 	}
-
 	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		List<String> result = new LinkedList<String>();
-		new Main2().build(result,"",n,n);
-		for(Iterator<String> it = result.iterator();it.hasNext();) {
-			System.out.print(it.next());
-			if(it.hasNext())
-				System.out.print(",");
-		}
+		float a = 12.5f;
+		double b = (int)Math.ceil(a);
+		System.out.print(b);
+//		Scanner scan = new Scanner(System.in);
+//		int n = scan.nextInt();
+//		int k = scan.nextInt();
+//		String num = scan.next();
+//		char max ='0',min='9';
+//		for(int i=0;i<n;i++){
+//			char now = num.charAt(i);
+//			if(now>max)
+//				max = now;
+//			if(now<min)
+//				min = now;
+//		}
+//		int min_cost =Integer.MAX_VALUE;
+//		String answer ="";
+//		for(int i=0;i<Math.pow((max-min),)
+
+
+
+
+
+
+//		List<String> result = new LinkedList<String>();
+//		new Main2().build(result,"",n,n);
+//		for(Iterator<String> it = result.iterator();it.hasNext();) {
+//			System.out.print(it.next());
+//			if(it.hasNext())
+//				System.out.print(",");
+//		}
 
 	}
 }
