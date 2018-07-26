9122a3e5f27c559247db26dc12ab38ec8fe0aa14
tracysaber <402807046@qq.com>
2018-07-26 23:02:40 +0800

    130

--- src/Leet_130.java
+++ src/Leet_130.java
@@ -20,33 +20,48 @@ import java.util.Comparator;
 
  */
 public class Leet_130 {
-	public boolean findO(char [][]b,int x,int y){
-		int row_len = b.length;
-		int col_len = b[0].length;
-		if(x==0||x==row_len-1||y==0||y==col_len-1){
-			//b[x][y]='X';
-			return true;
-		}
-
-		if(x>0&&b[x-1][y]=='O'){
-			b[x][y]='X';
-			if(findO(b,x-1,y)){
-				b[x][y]='O';
-			}
-		}
-		if(x>0&&y>0&&b[x+1][y+1]=='O'){
-
-		}
-		if(x>0&&y>0&&b[x+1][y+1]=='O'){
-
-		}
-		if(x>0&&y>0&&b[x+1][y+1]=='O'){
-
-		}
-		return false;
+	public void findO(char[][] b,int i,int j){
+		int rows = b.length,cols =0;
+		if(rows!=0) cols = b[0].length;
+		b[i][j] ='D';
+		if(i>0&&b[i-1][j]=='O')
+			findO(b,i-1,j);
+		if(j<cols-1&&b[i][j+1]=='O')
+			findO(b,i,j+1);
+		if(i<rows-1&&b[i+1][j]=='O')
+			findO(b,i+1,j);
+		if(j>0&&b[i][j-1]=='O')
+			findO(b,i,j-1);
 	}
 	public void solve(char[][] board) {
-		char [][] result = new char[board.length][board[0].length];
+		int rows = board.length,cols =0;
+		if(rows!=0) cols = board[0].length;
+		else return;
+		if(rows==1||cols==1)	return;
+		for(int i=0;i<cols-1;i++) {
+			if (board[0][i] == 'O') findO(board, 0, i);
 
+			if (board[rows-1][cols-i-1] == 'O') findO(board, rows-1, cols-i-1);
+
+		}
+		for(int i=0;i<rows-1;i++){
+			if (board[i][cols - 1] == 'O') findO(board, i, cols-1);
+			if (board[rows-i-1][0] == 'O') findO(board, rows-i-1, 0);
+		}
+		for(int i=0;i<rows;i++){
+			for(int j=0;j<cols;j++){
+				if(board[i][j]=='D')
+					board[i][j]='O';
+				else
+					board[i][j]='X';
+			}
+		}
+	}
+	public static void main(String args[]){
+		//char a[][] ={{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
+		char a[][] ={{'X','O','X','O','X','X'},{'O','X','O','X','O','X'},{'X','O','X','O','X','O'},{'O','X','O','X','O','X'}};
+		char b[][]={{'O'}};
+		new Leet_130().solve(b);
+		System.out.println();
 	}
 }
