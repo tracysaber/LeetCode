1dbc8542c5b1f86ac8078a4af818575dd07bdd74
tracysaber <402807046@qq.com>
2018-08-02 23:11:43 +0800

    1+1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main{
-
+    public void p(int n,int max,String now){
+        System.out.println(now+n);
+        for(int i = max;i<=n/2;i++){
+            p(n-i,i,now+i+'+');
+        }
+    }
+    public void Print(int n){
+        p(n,1,"");
+    }
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        
+        new Main().Print(8);
 
     }
 }
