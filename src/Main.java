7401da5c244893c0004b196a3887b5ff24daedae
tracysaber <402807046@qq.com>
2018-08-13 20:48:08 +0800

    main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
-import java.lang.reflect.Array;
 import java.util.*;
-
+class Card{
+    int x;
+    int y;
+    public Card(int f,int t){
+        this.x = f;
+        this.y = t;
+    }
+}
 public class Main{
-    public void p(int n,int max,String now){
-        System.out.println(now+n);
-        for(int i = max;i<=n/2;i++){
-            p(n-i,i,now+i+'+');
-        }
-    }
-    public void Print(int n){
-        p(n,1,"");
-    }
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        new Main().Print(8);
+        int n = scan.nextInt();
+        List<Card> cards = new ArrayList<Card>();
+        for(int i=0;i<n;i++){
+            cards.add(new Card(scan.nextInt(),scan.nextInt()));
+        }
+        Collections.sort(cards, new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                return o1.x-o2.x;
+            }
+        });
+        boolean flag = false;
+
+        Card old = cards.get(0);
+        int sum=old.y,min =Integer.MAX_VALUE,x_sum=old.x;
+        for(int i =1;i<n;i++){
+            Card now = cards.get(i);
+            x_sum+=now.x;
+            if(!flag&&old.x==now.x){
+                flag = true;
+            }
+            else{
+                flag=false;
+            }
+            if(now.x%2!=0&&now.y<min) min=now.y;
+            sum +=now.y;
+            old = now;
+        }
+        System.out.println(x_sum%2==0?sum:sum-min);
 
     }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -27,89 +27,31 @@
 //	}
 //}
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-import java.util.*;
-import java.util.concurrent.Executor;
-
-public class Main2 {
-//	public  void build(List<String> re,String now,int left,int right){
-//		if(left==0&&right==0){
-//			re.add(now);
-//		}
-//		else{
-//			if(left>0)	{
-//				now=now+"(";
-//				build(re,now,left-1,right);
-//				now=now.substring(0,now.length()-1);
-//			}
-//			if(right>0&&right>left){
-//				now=now+")";
-//				build(re,now,left,right-1);
-//			}
-//
-//		}
-//	}
-	public int getCost(String a,String b){
-		int cost =0;
-		for(int i=0;i<a.length();i++){
-			cost+=Math.abs(a.charAt(i)-b.charAt(i));
-		}
-		return cost;
+class time{
+	String from;
+	String to;
+	public time(String f,String t){
+		this.from = f;
+		this.to = t;
 	}
+}
+public class Main2 {
 	public static void main(String args[]){
-		float a = 12.5f;
-		double b = (int)Math.ceil(a);
-		System.out.println(b);
-		try{
-
-			InputStream inputStream = new FileInputStream("D:\\texlive\\release-texlive.txt");
-			byte byt[] = new byte[512];
-			inputStream.read(byt);
-			for(byte bb:byt)
-				System.out.print((char)bb);
-			inputStream.close();
-			Reader reader = new FileReader("D:\\texlive\\release-texlive.txt");
-			char ch[] = new char[512];
-			reader.read(ch);
-
-			for(char c:ch)
-				System.out.print(c);
-			//ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(""));
-			//out.writeObject();
+		Scanner scan  = new Scanner(System.in);
+		int t = scan.nextInt();
+		ArrayList<time> ts[] = new ArrayList[7];
+		for (int i=0;i<t;i++){
+			int k = scan.nextInt();
+			for(int j=0;j<k;j++){
+				int day = scan.nextInt();
+				int nums = scan.nextInt();
+				for(int jj=0;jj<nums;jj++){
+					
+				}
+			}
 		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-
-
-//		Scanner scan = new Scanner(System.in);
-//		int n = scan.nextInt();
-//		int k = scan.nextInt();
-//		String num = scan.next();
-//		char max ='0',min='9';
-//		for(int i=0;i<n;i++){
-//			char now = num.charAt(i);
-//			if(now>max)
-//				max = now;
-//			if(now<min)
-//				min = now;
-//		}
-//		int min_cost =Integer.MAX_VALUE;
-//		String answer ="";
-//		for(int i=0;i<Math.pow((max-min),)
-
-
-
-
-
-
-//		List<String> result = new LinkedList<String>();
-//		new Main2().build(result,"",n,n);
-//		for(Iterator<String> it = result.iterator();it.hasNext();) {
-//			System.out.print(it.next());
-//			if(it.hasNext())
-//				System.out.print(",");
-//		}
-
 	}
 }
